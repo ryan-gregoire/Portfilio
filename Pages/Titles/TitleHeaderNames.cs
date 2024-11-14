194ca6c29540e70cf607eab1a10aab3fde137158
RyanGregoire2024 <Ryangregoire286@gmail.com>
2024-11-14 14:23:53 -0500

    Add project files.

---
+++ Pages/Titles/TitleHeaderNames.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Name.Pages.Titles
+{
+    public class TitleHeaderNames : PageModel
+    {
+
+        public string? TitleHeaderMessage = "Ryan Gregoire Website | Home";
+        public string? PersonNameOFOwner = "I am - Ryan Gregoire";
+        public string? job = "Web Developer - Frontend Dev";
+        public string? headerTitle = "Ryan Gregoire";
+        public string? aboutTitleHeader = "Ryan Gregoire Website | About";
+        public string? footerTitleName = "2024 Ryan Gregoire | Websie";
+
+
+    }
+}
